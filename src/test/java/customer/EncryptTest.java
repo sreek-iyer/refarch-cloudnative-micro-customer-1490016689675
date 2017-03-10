e40dafcd4bc0de5b3d722d6cad87faa43aeedde5
Jeffrey Kwong <jkwong@Jeffreys-MacBook-Pro.local>
2017-03-10 12:03:16 -0500

    fix bug in tests

--- src/test/java/customer/EncryptTest.java
+++ src/test/java/customer/EncryptTest.java
@@ -28,41 +28,4 @@ public class EncryptTest {
 		assert(plaintext.equals(decrypted));
 		
 	}
-	
-	@Test
-	public void testMarshalFromJson() throws Exception {
-		final String id = "asdgadsfads";
-		
-		final ObjectMapper mapper = new ObjectMapper();
-		
-		// construct a json string with the above properties
-		
-		final StringBuilder myJsonStr = new StringBuilder();
-		
-		myJsonStr.append("{");
-		myJsonStr.append("\"customerId\":\"").append(id).append("\",");
-		myJsonStr.append("\"email\":").append("\"my@email.com\"").append(",");
-		myJsonStr.append("\"firstName\":").append("\"John\"").append(",");
-		myJsonStr.append("\"lastName\":").append("\"Smith\"").append(",");
-		myJsonStr.append("\"imageUrl\":").append("\"/image/myimage.jpg\"").append(",");
-		myJsonStr.append("\"username\":").append("\"user1\"").append(",");
-		myJsonStr.append("}");
-		
-		final String myJson = myJsonStr.toString();
-		System.out.println("My JSON String:" + myJson);
-		
-		// marshall json to Customer object
-		
-		final Customer inv = mapper.readValue(myJson, Customer.class);
-		
-		// make sure all the properties match up
-		assert(inv.getCustomerId().equals(id));
-		assert(inv.getFirstName().equals("John"));
-		assert(inv.getLastName().equals("Smith"));
-		assert(inv.getImageUrl().equals("/image/myimage.jpg"));
-		assert(inv.getUsername().equals("user1"));
-		assert(inv.getEmail().equals("my@email.com"));
-		
-		
-	}
 }
